87cbb29521e890ce361d87eb51d7589871a42185
Tigran Khachakranc <tigrankhachakranc@users.noreply.github.com>
2023-12-09 13:51:26 +0400

    Comments

--- Application.hpp
+++ Application.hpp
@@ -10,12 +10,12 @@ public:
     static Application& getInstance();
     static Document& getDocument();
     static ItemRegistry& getItemRegistry();
-    static bool& getQuit();
+    static bool& getQuit(); // TK: Do not do like this, you are breaking your encapsulation, instead you need simple Quit() method which will set your bool true 
     void exec();
 
 public:
     Application(const Application&) =delete;
-    Application& operator=(const Application&) =delete;
+    Application& operator=(const Application&) =delete; // TK: Good!
 
 private:
     Application();
@@ -24,7 +24,7 @@ private:
 private:
     CommandController controller_;
     Document document_;
-    ItemRegistry itemRegistry_;
+    ItemRegistry itemRegistry_; // TK: Actually you do not need Item registry, you need ShapeRegistry instead.
     bool quit_;
 };
 
